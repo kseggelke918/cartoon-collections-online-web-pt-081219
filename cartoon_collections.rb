2cfdd8bc3b734765a2597c571f05fd49da254a3a
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-19 18:57:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)
   array.each_with_index |dwarf, index|
-    "#{index} #{dwarf}""
+    "#{index} #{dwarf}"
+  end 
 end
 
 def summon_captain_planet# code an argument here
