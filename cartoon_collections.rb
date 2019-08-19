2d08870da5cd644d48b05f3c65af2f780b604d45
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-19 19:06:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.collect do |word| 
-    if word.length == 4 
+  array.collect {|word| word.length == 4}
+  if array[i] == true 
       true 
     else 
       false 
